e6bcdcc6b0d2e01c9ca8cc2d43661711204ab4e4
unknown <nandinisarkar079@gmail.com>
2024-05-02 20:58:06 -0400

    environment

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
+
 
 from app.application import Application
 
@@ -12,6 +15,43 @@ def browser_init(context):
     driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
     context.driver = webdriver.Chrome(service=service)
+
+    #driver_path = GeckoDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Firefox(service=service)
+
+    # BROWSERS WITH DRIVERS: provide path to the driver file ### not very using this function browser
+    #service = Service(executable_path='C:\Users\Nandini\python-selenium-automation\operadriver_win32')
+    #context.driver = webdriver.chrome(service=service)
+
+    ### SAFARI ###
+    # context.driver = webdriver.Safari()
+
+    ### HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
+    ### BROWSERSTACK ###
+    #Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'nandinisarkar_SA5mDb'
+    bs_key = 'RsMZ3Z1HxxvquWC5kV2q'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+
+    options = Options()
+    bstack_options = {
+        'os': 'Window',
+        'osVersion': '10',
+        'browserName': 'chrome',
+        'sessionName': 'User can open and close Terms and Conditions from sign in page'
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -34,6 +74,7 @@ def before_step(context, step):
 def after_step(context, step):
     if step.status == 'failed':
         print('\nStep failed: ', step)
+        context.app.base_page.save_screenshot(step)
 
 
 def after_scenario(context, feature):
